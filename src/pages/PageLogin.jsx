84c1f09f093977f1f81e5f38a6a434826a4c2061
Galina-S <galyna.sazonova@gmail.com>
2023-02-21 11:24:03 +0100

    Galina: login/register german-style

--- src/pages/PageLogin.jsx
+++ src/pages/PageLogin.jsx
@@ -17,10 +17,10 @@ export const PageLogin = (props) => {
             <br></br>
 		<form onSubmit={handleSubmit} className='login-form'>
             <label htmlFor="email">E-Mail-Adresse</label>
-            <input value = {email} onChange={(e)=> setEmail(e.target.value)} type="email" placeholder="user@gmail.com" id="email" name="email"/>
+            <input value = {email} onChange={(e)=> setEmail(e.target.value)} type="email"  id="email" name="email"/>
 
             <label htmlFor="password">Passwort</label>
-            <input value = {pass} onChange={(e)=> setPass(e.target.value)} type="password" placeholder="******" id="password" name="password"/>
+            <input value = {pass} onChange={(e)=> setPass(e.target.value)} type="password" id="password" name="password"/>
 
             <button className='btn-login' type="submit">Anmelden</button>
         </form>
