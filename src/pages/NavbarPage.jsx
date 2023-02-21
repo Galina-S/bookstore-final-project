0f74145a32c87541ea2e3b017651342a7c1af8ed
Nelly <akrabinelly@mail.ru>
2023-02-21 11:07:18 +0100

    NavbarPage and style

---
+++ src/pages/NavbarPage.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+
+import { NavLink, Routes, Route, Navigate} from "react-router-dom";
+import { PageBooks } from '../pages/PageBooks';
+import { PageHome } from '../pages/PageHome';
+import { PageRegister } from '../pages/PageRegister'
+import { PageLogin } from '../pages/PageLogin'
+
+
+export const NavbarPage = () => {
+  const [currentForm, setCurrentForm] = useState("login");
+  const [currentUser, setCurrentUser] = useState({
+    email: "",
+    password: "",
+    name: "",
+    accessGroups: [],
+  });
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  };
+
+
+  return (
+    <div>
+      <nav className="navbar1">
+        <div className="logo">
+          <NavLink to="/home">LOGO</NavLink>
+        </div>
+
+        <div className="auth">
+          <NavLink to="/login">Login</NavLink>
+          <NavLink to="/register">Register</NavLink>
+        </div>
+
+        {/* {currentForm ==='login' 
+              ? <PageLogin onFormSwitch = { toggleForm }/> 
+              : <PageRegister onFormSwitch = { toggleForm} /> } */}
+      </nav>
+
+      <nav className="navbar2">
+        <NavLink to="/books">Books</NavLink>
+      </nav>
+
+      <Routes>
+        <Route path="/books" element={<PageBooks />} />
+        <Route path="/home" element={<PageHome />} />
+        <Route path="/" element={<Navigate to="/home" />} />
+
+        <Route
+          path="/login"
+          element={<PageLogin setCurrentUser={setCurrentUser} />}
+        />
+        <Route
+          path="/register"
+          element={<PageRegister setCurrentUser={setCurrentUser} />}
+        />
+
+        {/* <Route path="/login" element={<PageLogin onFormSwitch = { toggleForm }/>}/>
+<Route path="/register" element={<PageRegister onFormSwitch = { toggleForm} />}/> */}
+      </Routes>
+    </div>
+  );
+};
--- src/pages/PageHome.jsx
+++ src/pages/PageHome.jsx
@@ -4,10 +4,16 @@
 export const PageHome = () => {
         return (
             <div className = "pageHome">
-                <h1>Books API</h1>
-                <ul>
-                    <li><a href="books">/books</a> -  List of  Books</li>
-                </ul>
+
+
+
+                
+               <div className="route">
+                    <h1>Books API</h1>
+                    <ul>
+                        <li><a href="books">/books</a> -  List of  Books</li>
+                    </ul>
+               </div>
             </div>
         )
     }
