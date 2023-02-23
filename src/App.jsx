4eea687ea4b8dd2a9c1b5c3db053d4265e29d862
Nelly <akrabinelly@mail.ru>
2023-02-23 22:53:55 +0100

    Conflikt is resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-
 import './App.scss';
+//import './App.css';
+
+
 import { NavbarPage } from './pages/NavbarPage';
+import { useContext } from 'react';
+import { AppContext } from './AppContext';
 
 //const baseUrl = import.meta.env.VITE_BACKEND_URL;
 
 function App() {
- 
+ const { currentUser } = useContext(AppContext);
+
   return (
     <div className="App">
+      <div className= "userArea">
+        Hezlich willkommen, {currentUser.username}
+      </div> 
       <NavbarPage/>
     </div>
   )
