ce56a83e47c5d6373d36b9954f1403ebeaad79c8
Mila Oktay <milavaspopova@gmail.com>
2023-03-07 12:31:11 +0100

    Search field redirection fixed

--- src/pages/PageBooks.jsx
+++ src/pages/PageBooks.jsx
@@ -18,7 +18,7 @@ export const PageBooks = () => {
   } = useContext(AppContext);
 
   useEffect(() => {
-    if (searchTerm !== "") {
+    if (searchTerm == "") {
       (async () => {
         loadBooks();
       })();
