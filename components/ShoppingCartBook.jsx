49c6a2a1abb96d26190def1e1f9adc4fdedca016
Mila Oktay <milavaspopova@gmail.com>
2023-03-23 19:51:33 +0100

    Cart page set

--- components/ShoppingCartBook.jsx
+++ components/ShoppingCartBook.jsx
@@ -1,5 +1,48 @@
+import { NavLink, Link } from "react-router-dom";
+import { useContext } from "react";
+import { AppContext } from "../src/AppContext";
+
 function ShoppingCartBook({ book }) {
-  return <div>{book.title}</div>;
+  const { cart, removeFromCart, increaseQty, decreaseQty } =
+    useContext(AppContext);
+
+  return (
+    <>
+      <div className="product">
+        <div className="product-image">
+          <NavLink to={`/books/${book._id}`} onClick={() => openSingleBook()}>
+            <img src={book.img} alt={book.title} />
+          </NavLink>
+        </div>
+        <div className="product-details">
+          <div className="product-title">
+            <NavLink to={`/books/${book._id}`} onClick={() => openSingleBook()}>
+              {book.title}
+            </NavLink>
+          </div>
+          <Link to={`/authors/${book.author}`}>
+            <div className="author">{book.author}</div>
+          </Link>
+        </div>
+        <div className="product-price">{book.price}</div>
+        <div className="product-quantity">{book.quantity}</div>
+        <div className="product-removal">
+          <div className="product-qty">
+            <button onClick={() => decreaseQty(book)}>-</button>
+
+            <button onClick={() => increaseQty(book)}>+</button>
+          </div>
+          <button
+            className="remove-product"
+            onClick={() => removeFromCart(book)}
+          >
+            Remove
+          </button>
+        </div>
+        <div className="product-line-price">{book.price * book.quantity}</div>
+      </div>
+    </>
+  );
 }
 
 export default ShoppingCartBook;
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -5,14 +5,51 @@ import ShoppingCartBook from "../../components/ShoppingCartBook";
 export const Cart = () => {
   const { cart } = useContext(AppContext);
   return (
-    <div>
+    <div className="shopping-cart">
+      <div className="column-labels">
+        <label className="product-image">Image</label>
+        <label className="product-details">Product</label>
+        <label className="product-price">Price</label>
+        <label className="product-quantity">Quantity</label>
+        <label className="product-removal">Remove</label>
+        <label className="product-line-price">Total</label>
+      </div>
       {cart.map((_book) => {
         return (
-          <div className="book" key={_book._id}>
+          <div key={_book._id}>
             <ShoppingCartBook book={_book} />
           </div>
         );
       })}
+      <div className="totals">
+        <div className="totals-item">
+          <label>Total</label>
+          <div className="totals-value" id="cart-subtotal">
+            {cart.reduce((total, cartItem) => {
+              return total + (cartItem.price || 0) * cartItem.quantity;
+            }, 0)}
+          </div>
+        </div>
+        {/* <div className="totals-item">
+          <label>Tax (5%)</label>
+          <div className="totals-value" id="cart-tax">
+            3.60
+          </div>
+        </div>
+        <div className="totals-item">
+          <label>Shipping</label>
+          <div className="totals-value" id="cart-shipping">
+            15.00
+          </div>
+        </div>
+        <div className="totals-item totals-item-total">
+          <label>Grand Total</label>
+          <div className="totals-value" id="cart-total">
+            90.57
+          </div>
+        </div>*/}
+      </div>
+      <button className="checkout">Checkout</button>
     </div>
   );
 };
