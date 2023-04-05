ca86acbee017820c290d50a054be586e465028b9
Galina-S <galyna.sazonova@gmail.com>
2023-04-05 10:18:36 +0200

    cors

--- components/axios.jsx
+++ components/axios.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 // import dotenv from 'dotenv';
 
 // dotenv.config();
-export const BACKEND_URL= 'https://poised-elk-singlet.cyclic.app';
+export const BACKEND_URL= 'https://elegant-rose-outerwear.cyclic.app';
 
 const instance = axios.create({
     BACKEND_URL: BACKEND_URL
